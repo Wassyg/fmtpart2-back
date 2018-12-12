053645323b4c153999ff7b94cb0cecc29e4b8097
Wassy Guerro <wguerrouani@gmail.com>
2018-12-12 19:01:30 +0100

    change in Data models

--- databasescripts/artistDbCreation.js
+++ databasescripts/artistDbCreation.js
@@ -16,26 +16,13 @@ mongoose.connect('mongodb://'+dbuser+':'+dbpassword+'@ds039301.mlab.com:39301/fi
 );
 
 cloudinary.config({
-  cloud_name: "crazycloud",
-  api_key: '255876528863486',
-  api_secret: '0qzSisIetVmja-LecM_n0PiH-CQ'
+  cloud_name: "fulltattooedjacket",
+  api_key: '457899133511257',
+  api_secret: '7YCQsMJ8YUBk1a2-EZHfcD4xV7E'
 });
 
-//// ARTIST SCHEMA ////
-var artistSchema = mongoose.Schema({
-    artistNickname: String,
-    artistCompanyName: String,
-    artistAddress: String,
-    artistDescription: String,
-    artistAddressLat: Number,
-    artistAddressLon: Number,
-    artistEmail:String,
-    artistPhotoLink : String,
-    artistStyleList : [String],
-    artistNote : Number,
-});
-var ArtistModel = mongoose.model('artists', artistSchema);
-
+//// LOAD ARTIST SCHEMA ////
+const Artist = require('../routes/models/Artist');
 
 //// INITIAL DATABASE ////
 var ArtistDB = [
@@ -43,9 +30,9 @@ var ArtistDB = [
     artistNickname : "Bichon",
     artistCompanyName : "The Golden Rabbit Tattoo",
     artistAddress: "10 Rue Gambey, 75011 Paris",
-    artistDescription: "Bichon tatoue depuis 10 ans. Il a commencé à l'âge de 14 ans avec sa grande soeur. Depuis il est passionné de tatouages.",
+    artistDescription: "Après un passage par le graphisme, Bichon s'est créé son propre style : épuré, fluide et élégant.",
     artistEmail: "bichontatoueur@gmail.com",
-    artistComputerPhotoLink : "../FindMyTattooFront/public/avatarsTatoueurs/11201563_749803451831654_737090053_a.jpg",
+    artistComputerPhotoLink : "../public/avatarsTatoueurs/bichon.jpg",
     artistStyleList : ["DotWork", "FineLine", "BlackWork"],
     artistNote : 5,
     },
@@ -53,97 +40,70 @@ var ArtistDB = [
    artistNickname : "Princess Madness",
    artistCompanyName : "Lez'art du Corps - Paris",
    artistAddress: "16 Rue Geoffroy-Marie, 75009 Paris",
-   artistDescription: "Princess Madness s'est d'abord lancée dans la mode. Elle aime le style cartoon et découvrir des belles personnalités.",
+   artistDescription: "Princess Madness s'est d'abord lancée dans la mode. Elle aime le style cartoon et les créations déjantées.",
    artistEmail: "princess-madness@hotmail.com",
-   artistComputerPhotoLink : "../FindMyTattooFront/public/avatarsTatoueurs/41450515_1897257143642841_5668628696324374528_n.jpg",
+   artistComputerPhotoLink : "../public/avatarsTatoueurs/princessM.jpg",
    artistStyleList : ["Cartoon", "NewSchool", "Postmodern"],
    artistNote : 5,
  }
 ];
 
-//// LINKS OF ARTISTS TO SCRAP ////
-var artistLink = [
-  "https://www.tattoome.com/fr/org/2873/lagrif-bleue",
-  "https://www.tattoome.com/fr/org/134/kalie-art-tattoo#",
-  "https://www.tattoome.com/fr/org/2901/marlene-le-cidre-",
-  "https://www.tattoome.com/fr/org/2872/chez-meme",
-  "https://www.tattoome.com/fr/org/236/soul-vision-tattoo"
-];
 
 //// USEFUL FUNCTION TO DELAY LAUNCH OF FOLLOWING FUNCTION ////
 const timeoutPromise = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
 
-//// ARTIST SCRAPING FUNCTION ////
-var scraper = function(url, artistDatabase){
-  request(url,
-    function(err, response ,body){
-      var $ = cheerio.load(body);
-      //Create new artist
-      var element = {
-        artistNickname : $('.studio_tatoueurs li:nth-child(1):first-child').text(),
-        artistCompanyName : $('.studio_content_header span:first-child').text(),
-        artistAddress:
-          $('.studio_localisation_adresse p:nth-child(3)').text()+" "+
-          $('.studio_localisation_adresse p:nth-child(4)').text()+" "+
-          $('.studio_localisation_adresse p:nth-child(5)').text(),
-        artistDescription : $('.tab-pane .description').first().text().substring(0,120)+"...",
-        artistEmail: $('.studio_tatoueurs li:nth-child(1):first-child').text().toLowerCase()+ "@fakemail.com",
-        artistComputerPhotoLink : "https://www.tattoome.com"+$('.carousel-inner img')[0].attribs.src,
-        artistStyleList : [
-          $('.tab-pane .resultat_content_texte_tag:nth-child(2)').first().text(),
-          $('.tab-pane .resultat_content_texte_tag:nth-child(3)').first().text(),
-          $('.tab-pane .resultat_content_texte_tag:nth-child(4)').first().text()
-        ],
-        artistNote : Math.floor(Math.random()*2)+3,
-      }
-      artistDatabase.push(element);
-      console.log(artistDatabase);
-    }
-  )
-}
-// scraper("https://www.tattoome.com/fr/org/2873/lagrif-bleue", []);
 
-//// DATABASE ENRICHING (GPS COORD AND CLOUDINARY) FUNCTION ////
-//// Enrichissement de la base de données tatoueurs avec coordonnées GPS ////
-// To learn more on how to convert addresses to coordinates, check this simple website : https://dzone.com/articles/mapboxs-api-to-geocode-data-to-get-location-inform
-var enrichingArtistDB = async function(artistDatabase){
-  var artistDatabaseAddress=artistDatabase.map(a=>a.artistAddress);
-  console.log(artistDatabaseAddress);
-  for (var i = 0; i < artistDatabaseAddress.length; i++) {
-    request('https://api.mapbox.com/geocoding/v5/mapbox.places/'+ artistDatabaseAddress[i]+'.json?access_token=pk.eyJ1IjoiZml0emZvdWZvdSIsImEiOiJjam9nMGlkMXowOTkzM3h0N3E5am45b3hxIn0.IBgvst88EucTyqijWWnpSg', function(error, response, body){
-      addressInfo=JSON.parse(body);
-      artistDatabase[i].artistAddressLat = addressInfo.features[0].center[1];
-      artistDatabase[i].artistAddressLon = addressInfo.features[0].center[0];
-      cloudinary.v2.uploader.upload(artistDatabase[i].artistComputerPhotoLink, {public_id: "artists/artist_"+i}, function(error, result){
-        console.log(result.secure_url, error);
-        artistDatabase[i].artistPhotoLink = result.secure_url;
-        console.log(artistDatabase[i]);
-        var newArtist = new ArtistModel (artistDatabase[i]);
-        newArtist.save(
-          function (error, artist) {
-            console.log(artist);
+
+//Fonction pour ajouter adresse et coordonnées à un artiste
+     // function addArtistCoordinates(artist){
+     //   return new Promise((resolve, reject)=>{
+     //     request('https://api.mapbox.com/geocoding/v5/mapbox.places/'+artist.artistAddress+'.json?access_token=pk.eyJ1IjoiZml0emZvdWZvdSIsImEiOiJjam9nMGlkMXowOTkzM3h0N3E5am45b3hxIn0.IBgvst88EucTyqijWWnpSg',
+     //     function(err, response, body){
+     //       addressInfo=JSON.parse(body);
+     //       artist.artistAddressLat = addressInfo.features[0].center[1];
+     //       artist.artistAddressLon = addressInfo.features[0].center[0];
+     //       //console.log("artist 150", artist)
+     //       resolve(artist);
+     //     })
+     //   })
+     // }
+
+function pushArtistPhotoOnCloudinaryAndSaveItInMLab() {
+  ArtistDB.map((artist, i) =>{
+    let photoPath = artist.artistComputerPhotoLink
+         cloudinary.v2.uploader.upload(photoPath,
+          {public_id: "artists/artist_"+i},
+           function(error, result){
+          if (error) {
+            return console.log(error);
+          } else {
+            var newArtist = new ArtistModel ({
+               artistNickname: artist.artistNickname,
+               artistCompanyName: artist.artistCompanyName,
+               artistAddress: artist.artistAddress,
+               artistDescription: artist.artistDescription,
+               artistAddressLat: artist.artistAddressLat,
+               artistAddressLon: artist.artistAddressLon,
+               artistEmail: artist.artistEmail,
+               artistPhotoLink : result.secure_url,
+               artistStyleList : [{
+                 style1: artist.artistStyleList[0],
+                 style2: artist.artistStyleList[1],
+                 style3: artist.artistStyleList[2]
+               }],
+               artistNote : artist.artistNote,
+               favArtistID: artist.favArtistID
+             });
+
+            newArtist.save(
+              function(error, artist) {
+                console.log(error, artist);
+          })
           }
-        );
-      });
-    });
-    console.log(i);
-    await timeoutPromise(4000);
-  }
+  })
+})
 }
 
-// console.log(enrichingArtistDB(ArtistDB));
-
-//// FUNCTION TO CREATE ARTIST DATABASE ON MONGO DB WITH ALL INFO ////
-async function createArtistDB() {
-  for (var i = 0; i < artistLink.length; i++) {
-    await timeoutPromise(1000);
-    scraper(artistLink[i], ArtistDB);
-  }
-  await timeoutPromise(5000);
-  console.log(ArtistDB);
-  enrichingArtistDB(ArtistDB);
-}
 
 ////// TO LAUNCH THE CREATION OF DATABASE : UNCOMMENT THE LINE BELOW /////
-
-// createArtistDB();
+//pushArtistPhotoOnCloudinaryAndSaveItInMLab();
--- databasescripts/tattooDbCreation.js
+++ databasescripts/tattooDbCreation.js
@@ -16,34 +16,15 @@ mongoose.connect('mongodb://'+dbuser+':'+dbpassword+'@ds039301.mlab.com:39301/fi
 );
 
 cloudinary.config({
-  cloud_name: "crazycloud",
-  api_key: '255876528863486',
-  api_secret: '0qzSisIetVmja-LecM_n0PiH-CQ'
+  cloud_name: "fulltattooedjacket",
+  api_key: '457899133511257',
+  api_secret: '7YCQsMJ8YUBk1a2-EZHfcD4xV7E'
 });
 
-// Artist DB
-var artistSchema = mongoose.Schema({
-    artistNickname: String,
-    artistCompanyName: String,
-    artistAddress: String,
-    artistDescription: String,
-    artistAddressLat: Number,
-    artistAddressLon: Number,
-    artistEmail:String,
-    artistPhotoLink : String,
-    artistStyleList : [String],
-    artistNote : Number,
-});
-var ArtistModel = mongoose.model('artists', artistSchema);
 
-// Tattoo DB
-var tattooSchema = mongoose.Schema({
-    tattooPhotoLink: String,
-    tattooStyleList: [String],
-    artistID:String,
-});
-var TattooModel = mongoose.model('tattoos', tattooSchema);
-
+//load User model, Artist model, Lead model, Tattoo model
+const Artist = require('../routes/models/Artist');
+const Tattoo = require('../routes/models/Tattoo');
 
 //// INITIAL DATABASE ////
 
@@ -60,58 +41,58 @@ function tattooDBCreationWithLocalImages(path, artistNickname){
     resolve(tattooDB);
   });
 }
-// Function to get new tattoo info from scraping
-function getNewElementByScraping(url, photoIndex){
-  return new Promise((resolve, reject) => {
-    request(url,
-      function(err, response ,body){
-        let $ = cheerio.load(body);
-        let element = {
-          tattooComputerPhotoLink : "https://www.tattoome.com"+$('.carousel-inner img')[photoIndex].attribs.src,
-          artistNickname : $('.studio_tatoueurs li:nth-child(1):first-child').text(),
-        }
-        resolve(element);
-      }
-    )
-  });
-}
+// Function to get new tattoo info from scraping photoIndex is for each picture coming from the same artist
+// function getNewElementByScraping(url, photoIndex){
+//   return new Promise((resolve, reject) => {
+//     request(url,
+//       function(err, response, body){
+//         let $ = cheerio.load(body);
+//         let element = {
+//           tattooComputerPhotoLink : "https://www.tattoome.com"+$('.carousel-inner img')[photoIndex].attribs.src,
+//           artistNickname : $('.studio_tatoueurs li:nth-child(1):first-child').text(),
+//         }
+//         resolve(element);
+//       }
+//     )
+//   });
+// }
 // Function to add to a database several tattoos from several artists from scraping
-async function tattooDBScrapping(scrappingLinks, database){
-  // Go through several artists url pages
-  for (let i = 0; i < scrappingLinks.length; i++) {
-    // Go through several photos of one artist
-    for (let j = 1; j < 6; j++) {
-      let newElement = await getNewElementByScraping(scrappingLinks[i], j);
-      database.push(newElement);
-    }
-  }
-  console.log(database);
-}
+// async function tattooDBScrapping(scrappingLinks, database){
+//   // Go through several artists url pages
+//   for (let i = 0; i < scrappingLinks.length; i++) {
+//     // Go through several photos of one artist
+//     for (let j = 1; j < 6; j++) {
+//       let newElement = await getNewElementByScraping(scrappingLinks[i], j);
+//       database.push(newElement);
+//     }
+//   }
+//   console.log(database);
+// }
 
 //// LINKS OF ARTISTS TO SCRAP ////
-var tattooLink = [
-  "https://www.tattoome.com/fr/org/2873/lagrif-bleue",
-  "https://www.tattoome.com/fr/org/134/kalie-art-tattoo#",
-  "https://www.tattoome.com/fr/org/2901/marlene-le-cidre-",
-  "https://www.tattoome.com/fr/org/2872/chez-meme",
-  "https://www.tattoome.com/fr/org/236/soul-vision-tattoo"
-];
+// var tattooLink = [
+//   "https://www.tattoome.com/fr/org/2873/lagrif-bleue",
+//   "https://www.tattoome.com/fr/org/134/kalie-art-tattoo#",
+//   "https://www.tattoome.com/fr/org/2901/marlene-le-cidre-",
+//   "https://www.tattoome.com/fr/org/2872/chez-meme",
+//   "https://www.tattoome.com/fr/org/236/soul-vision-tattoo"
+// ];
 
 // Function to concatenate the two ways of collecting tattoos
 function tattooDBCreation() {
   return new Promise(async function(resolve, reject) {
     //Creation of database with local files
-    const TattooDBBichon = await tattooDBCreationWithLocalImages('../../FindMyTattooFront/public/tatouagesBichon/', 'Bichon');
-    const TattooDBPrincess = await tattooDBCreationWithLocalImages('../../FindMyTattooFront/public/tatouagesPrincess/', 'Princess Madness');
+    const TattooDBBichon = await tattooDBCreationWithLocalImages('../public/images/tatouagesBichon/', 'Bichon');
+    const TattooDBPrincess = await tattooDBCreationWithLocalImages('../public/images/tatouagesPrincess/', 'Princess Madness');
     let TattooDB = TattooDBBichon.concat(TattooDBPrincess);
     // Creation of database with web files from SCRAPING
-    tattooDBScrapping(tattooLink, TattooDB);
+    //tattooDBScrapping(tattooLink, TattooDB);
     console.log(TattooDB);
     resolve (TattooDB)
   });
 }
 
-// tattooDBCreation();
+ //tattooDBCreation();
 
 
 
@@ -160,13 +141,12 @@ const timeoutPromise = (timeout) => new Promise((resolve) => setTimeout(resolve,
 async function tattooDBEnrichment() {
   let TattooDB = await tattooDBCreation();
   await timeoutPromise(60000);
-  TattooDB.forEach(async function(tattoo, i){
+  TattooDB
+  .forEach(async function(tattoo, i){
     let tattooWithCloud = await pushTattooPhotoOnCloudinary(tattoo, i);
-    console.log("on cloud");
     let tattooWithArtist = await addArtistInfoToTattoo(tattooWithCloud, tattooWithCloud.artistNickname);
-    console.log("with artist");
     await createNewTattooInDB(tattooWithArtist);
   })
 }
 
-tattooDBEnrichment();
+//tattooDBEnrichment();
